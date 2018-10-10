2a4667eeb53f2cfcc9a6d0e6f7514ea6729dfc07
sinosofti <825407457@qq.com>
2018-10-10 10:08:52 +0800

    vue

---
+++ vue-framework-wz-master/src/views/components/wz.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="animated fadeIn">
+
+    <Row>
+      <Col :sm="24" :md="24" :lg="12">
+
+        <h3>框架在手，天下我有</h3>
+        <p>好用的框架决定了一个程序员的效率</p>
+
+      </Col>
+    </Row>
+    
+  </div>
+</template>
+
+   
+<script>
+
+    export default {
+        data () {
+            return {
+        　　　　　　　　}
+      }
+       
+    }
+</script>
